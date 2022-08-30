ee30f9543e760992ea0accad3f26764aba2670ea
Bill Kuker <bkuker@billkuker.com>
2022-08-30 08:11:18 -0400

    Empty program

---
+++ Variables.cpp
@@ -0,0 +1,12 @@
+//Include the "IO Stream" library. It includes the cout functions
+#include <iostream>
+using std::cout;
+
+//Include the "String" library, that defines the string type
+#include <string>
+using std::string;
+
+void main(){
+   //Our program starts executing at the beginning of the main function
+   
+}
